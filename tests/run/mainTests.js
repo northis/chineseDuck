2080a0b942ced6c2863782d49c7a4cf512e18229
Mikhail Berdnikov <m.berdnikov@adguard.com>
2018-11-14 19:07:37 +0300

    test integration

--- tests/run/mainTests.js
+++ tests/run/mainTests.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 import * as mongooseTests from "./mongooseTests";
 import * as webApiTests from "./webApiTests";
 import * as srv from "../../src/server";
+import { execSync } from "child_process";
+import { Settings } from "../../config/common";
 
 export default async () => {
   describe("entire tests", () => {
@@ -11,6 +13,7 @@ export default async () => {
 
     describe("mongoose models set of tests", () => mongooseTests.default());
     describe("web api tests", () => webApiTests.default());
+    describe("netcore bot service tests", () => execSync("dotnet test ../../bot/chineseDuck.pinyin4net.tests/chineseDuck.pinyin4net.tests.cproj -c Release -f netcoreapp2.1 -apiAddress " + Settings.getLocalApiAddress()));
 
     after(async () => {
       await srv.default.shutDown();
