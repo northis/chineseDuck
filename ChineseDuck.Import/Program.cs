bce72e8d4315a73d985dc52f4fa950754ff98256
northis <north@live.ru>
2018-12-03 15:04:04 +0300

    import utility

---
+++ ChineseDuck.Import/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace ChineseDuck.Import
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
