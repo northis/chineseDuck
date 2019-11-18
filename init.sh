0e7463aea190df71e5b8c02296d14718abf3c907
Mikhail Berdnikov <north@live.ru>
2019-11-18 23:47:44 +0300

    f/32, +bot settings

--- init.sh
+++ init.sh
@@ -13,8 +13,10 @@ sed -e "s/USER:PASSWORD/$MONGO_API_USER:$MONGO_API_PASSWORD/;\
 keys.template.js > keys.js
 
 #replace appsettings.json
-sed -e "s/webpartsite.com/$PUBLIC_URL/; s/TELEGRAM_BOT_KEY/$TELEGRAM_BOT_KEY/;\
-s/PWD/$SERVICE_COMMAND/" appsettings.template.json > appsettings.json
+sed -e "s/webpartsite.com/$PUBLIC_URL/; s/TELEGRAM_BOT_KEY/$TELEGRAM_BOT_KEY/; \
+s/PWD/$SERVICE_COMMAND/; s/(\"AdminUserId\":\s)(\d+)/\1$ADMIN_USER_ID/; \
+s/(\"ServerUserId\":\s)(\d+)/\1$SERVER_USER_ID/; s/(\"UserId\":\s)(\d+)/\1$USER_ID/"\ 
+appsettings.template.json > appsettings.json
 
 #replace db.js
 sed -e "s/adminPassword/$MONGO_ADMIN_PASSWORD/;\
